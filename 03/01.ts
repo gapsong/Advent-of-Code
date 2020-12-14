bf072518b77d8630e6103a997f7fe536ccb6efae
Khiem Ton-That <khiem.ton-that@jameda.de>
2020-12-14 21:51:07 +0100

    submitted day 3 task 1

---
+++ 03/01.ts
@@ -0,0 +1,19 @@
+export {};
+
+const fileContents = Deno.readTextFileSync('./input');
+const map = fileContents.split('\n');
+const elementsPerRow = fileContents.split('\n')[0].length;
+
+let treeCounter = 0;
+let x = 3;
+const detectCollision = (x: number, y: number) => {
+  if (map[y][x % elementsPerRow] == '#') {
+    treeCounter++
+  }
+};
+
+for (var y = 1; y < map.length; y++, x = x + 3) {
+  detectCollision(x, y);
+}
+
+console.log('treeCounter', treeCounter);
