fa3fc4ee07fe2a8ec2491c064f7294b8bb2067b1
gapsong <khiemtonthat@yahoo.de>
2020-12-17 00:49:46 +0100

    some error somewhere. I dont know

--- 04/02.ts
+++ 04/02.ts
@@ -6,62 +6,89 @@ const passports = fileContents.split('\n\n');
 
 const toBoolNumber = (value: number) => value >= 0;
 
-const validChars = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-];
+const validNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const validChars = [...validNumbers, 'a', 'b', 'c', 'd', 'e', 'f'];
+const validEyeClr = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
 
 const validateAttributes = (attribute: string): boolean => {
-  const vals = attribute.split(':');
+    const vals = attribute.split(':');
 
-  const key = vals[0];
-  const value = vals[1];
+    const key = vals[0];
+    const value = vals[1];
 
-  if (key == 'byr') {
-    return 1920 <= parseInt(value) && parseInt(value) <= 2002;
-  }
-
-  if (key == 'iyr') {
-    return 2010 <= parseInt(value) && parseInt(value) <= 2020;
-  }
-
-  if (key == 'eyr') {
-    return 2020 <= parseInt(value) && parseInt(value) <= 2030;
-  }
-
-  if (key == 'hcl') {
-    if (value.split('')[0] == '#' && value.split('').length == 7) {
-      let acc = true
-      value.split('').map((item) => {
-        if(!acc){
-          console.log(item)
+    switch (key) {
+        case 'cid': {
+            return true;
         }
-        acc = acc && validChars.includes(item);
-      }, true);
+        case 'byr': {
+            console.log('byr', value, 1920 <= parseInt(value) && parseInt(value) <= 2002);
+            return 1920 <= parseInt(value) && parseInt(value) <= 2002;
+        }
 
-      return acc
+        case 'iyr': {
+            console.log('iyr', value, 2010 <= parseInt(value) && parseInt(value) <= 2020);
+
+            return 2010 <= parseInt(value) && parseInt(value) <= 2020;
+        }
+
+        case 'eyr': {
+            console.log('eyr', value, 2020 <= parseInt(value) && parseInt(value) <= 2030);
+
+            return 2020 <= parseInt(value) && parseInt(value) <= 2030;
+        }
+
+        case 'hcl': {
+            const valueAtt = value.split('');
+            if (value.split('')[0] == '#' && value.split('').length == 7) {
+                const temp = valueAtt.splice(1, valueAtt.length).reduce((acc, item) => {
+                    return acc && validChars.includes(`${item}`);
+                }, true);
+                console.log('hcl', value, temp);
+
+                return temp;
+            }
+        }
+
+        case 'ecl': {
+            console.log('ecl', value, validEyeClr.includes(value));
+
+            return validEyeClr.includes(value);
+        }
+
+        case 'pid': {
+            const expoldedValue = value.split('');
+            const isNumbers = expoldedValue.reduce((acc, item) => {
+                return acc && validNumbers.includes(`${item}`);
+            }, true);
+
+            console.log('pid', value, value.split('').length == 9 && isNumbers);
+            return value.split('').length == 9 && isNumbers;
+        }
+
+        case 'hgt': {
+            if (value.search('cm') != -1) {
+                const height = value.split('cm')[0];
+                return 150 <= Number(height) && Number(height) <= 193;
+            }
+            if (value.search('in') != -1) {
+                const height = value.split('in')[0];
+                console.log('in', height, 59 <= Number(height) && Number(height) <= 76);
+                return 59 <= Number(height) && Number(height) <= 76;
+            }
+        }
     }
-  }
-
-  return false
+    return false;
 };
 
 const validPassports = passports.filter((passport) => {
-  const passportAttributes = passport.split(/ |\n/).map(validateAttributes);
+    const passportAttributes = passport.split(/ |\n/).map(validateAttributes);
+    console.log(passportAttributes);
+    const temp = passportAttributes.reduce((acc, item) => {
+        return acc && item;
+    }, true);
+
+    console.log(temp);
+    return temp;
 });
 
-console.log(validateAttributes('hcl: #12345f'));
+console.log(validPassports.length);
