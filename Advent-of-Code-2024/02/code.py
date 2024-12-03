10b8e264e026d746a4dcfcf69a0dba3393e866c7
Khiem <khiemtonthat@yahoo.de>
2024-12-03 19:39:02 +0100

    solved 02.2

--- Advent-of-Code-2024/02/code.py
+++ Advent-of-Code-2024/02/code.py
@@ -12,20 +12,32 @@ except FileNotFoundError:
 
 
 result = 0
+
+
+def is_safe_check(my_list):
+    if sorted(my_list) != my_list and sorted(my_list, reverse=True) != my_list:
+        return False
+    for i in range(len(my_list) - 1):
+        diff = abs(my_list[i] - my_list[i + 1])
+        if diff < 1 or diff > 3:
+            return False
+    return True
+
+
 for sample in listing[:-1]:
     cur = 0
     sample_list = sample.split(" ")
     sample_list = [int(item) for item in sample_list[:]]
 
-    if sorted(sample_list) != sample_list and sorted(sample_list, reverse=True) != sample_list:
-        continue
-
-    for i in range(len(sample_list) - 1):
-        diff = abs(sample_list[i] - sample_list[i + 1])
-        if diff < 1 or diff > 3:
-            break
+    if is_safe_check(sample_list):
+        result += 1
     else:
-        if max(sample_list) == sample_list[-1] and min(sample_list) == sample_list[0] or min(sample_list) == sample_list[-1] and max(sample_list) == sample_list[0]:
-            result += 1
+        for i in range(len(sample_list)):
+            copy = sample_list[:]
+            del copy[i]
+            if is_safe_check(copy):
+                result += 1
+                print(copy)
+                break
 
 print(result)
